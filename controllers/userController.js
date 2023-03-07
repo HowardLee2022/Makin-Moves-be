0bd00e196fd3f64c916bacc6efbf70c9bc7e8f6a
nguyet33 <tdnguyet33@gmail.com>
2023-03-07 12:37:35 -0800

    user w/trips

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const router = express.Router();
-const {User} = require('../models');
+const {User, Trips} = require('../models');
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
@@ -92,13 +92,28 @@ router.post("/", (req, res) => {
     }
   });
 
+  //GET ALL USERS
 router.get("/",(req,res)=>{
     User.findAll().then(userData=>{
      res.json(userData)
     }).catch(err=>{
      console.log(err);
-     res.status(500).json({msg:"oh noes!",err})
+     res.status(500).json({msg:"Cannot get all users!",err})
     })
  })
+
+ //GET USERS W/ TRIPS
+ router.get("/:id", (req, res) => {
+  User.findByPk(req.params.id, {
+    include: [Trips],
+  })
+    .then((userData) => {
+      res.json(userData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({ msg: "oh no", err });
+    });
+});
 
  module.exports = router;
